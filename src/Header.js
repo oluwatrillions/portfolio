fe589afd25771805fc9ef09f06839e09fab318f1
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2021-12-06 11:58:18 +0100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Projects</li>
+                        <li>Contact Me</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import images from './images/backg.jpg'
+import './Hero.css'
+
+function Hero() {
+    return (
+        <div>
+            <img src={images} alt='' />
+            <h1>Hello, I'm Ajose Michael Oluwatobi</h1>
+        </div>
+    )
+}
+
+export default Hero
