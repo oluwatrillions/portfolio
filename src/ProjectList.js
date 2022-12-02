f273068c54029eb6ff88111186951ec2b124751e
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-12-02 16:02:21 +0100

    ..

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,17 +2,17 @@ const Project = [
     {
         id: 1,
         name: 'Calculator',
-        image: require("./images/calculator.gif")
+        image: require("./images/calculator.gif").default
     },
     {
         id: 2,
         name: 'Quiz App',
-        image: require('./images/quiz-app.gif')
+        image: require('./images/quiz-app.gif').default
     }, 
     {
         id: 3,
         name: 'Landing Page',
-        image: require('./images/landing page.gif')
+        image: require('./images/landing page.gif').default
     }
 ]
 
