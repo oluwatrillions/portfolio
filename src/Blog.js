26ef38b00cfe7b64f63013b70bbba156ed8a3646
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-12-02 15:26:31 +0100

    blog page first commit

--- src/Blog.js
+++ src/Blog.js
@@ -4,7 +4,22 @@ import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+      <div className='blog-container'>
+          <div className='blog-title'>
+                Welcome to my blogpost. Do have a great read.
+          </div>
+          <div className='blog-lists'>
+              <div className='left-side'>
+                  
+              </div>
+              <section className='blog-page'>
+                  
+              </section>
+              <div className='right-side'>
+                  
+              </div>
+          </div>
+      </div>
   )
 }
 
