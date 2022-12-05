6f4fad12d353673584efea34371a606d68e2eb9d
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-12-05 09:58:56 +0100

    project error

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,12 +2,12 @@ const Project = [
     {
         id: 1,
         name: 'Calculator',
-        image: require("./images/calculator.gif")
+        image: require("./images/calculator.gif").default
     },
     {
         id: 2,
         name: 'Quiz App',
-        image: require('./images/quiz-app.gif')
+        image: require('./images/quiz-app.gif').default
     }, 
     {
         id: 3,
