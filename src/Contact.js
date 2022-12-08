d64770a71546956462a2ec6b88ccdcf3059eb454
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-12-08 03:31:07 +0100

    contact me redesign

--- src/Contact.js
+++ src/Contact.js
@@ -1,49 +1,67 @@
 import React from 'react'
 import './Contact.css'
-import linkedin from './images/linkedin.png'
-import github from './images/github.png'
-import twitter from './images/twitter.png'
-import yahoo from './images/yahoo.png'
-import instagram from './images/instagram.png'
-import phone from './images/phone.png'
+import {FaLinkedin} from "react-icons/fa";
+import {FaInstagramSquare} from "react-icons/fa";
+import {FaTwitterSquare} from "react-icons/fa";
+import {FaGithubSquare} from "react-icons/fa";
+import {FaPhoneSquare} from "react-icons/fa";
 
 function Contact() {
     return (
         <div className='contact'>
-            <p>I am based in <span>Lagos, Nigeria, and open to relocation</span>. If you have any questions to ask<br />
+            <section className='contact-sec'>
+                <h1>Contact Me</h1>
+                <p>I am based in <span>Lagos, Nigeria, and open to relocation</span>. If you have any questions to ask<br />
                 or would like to work with me, I am reachable through any of<br />
                 the below medium:</p>
-            <div className='medium'>
-                <div className='media'>
-                    <img src={linkedin} alt='' />
-                    <a href='https://www.linkedin.com/in/oluwatrillions/'>@oluwatrillions</a>
-                </div>
-                 <div className='media'>
-                    <img src={github} alt='' />
-                    <a href='https://github.com/oluwatrillions'>@oluwatrillions</a>
-                </div>
-                 <div className='media'>
-                    <img src={twitter} alt='' />
-                    <a href='https://twitter.com/Oluwatrillions'>@oluwatrillions</a>
-                </div>
-                 <div className='media'>
-                    <img src={instagram} alt='' />
-                    <a href='https://instagram.com/oluwatrillion'>@oluwatrillions</a>
-                </div>
-                 <div className='email'>
-                    <img src={yahoo} alt='' />
-                    <h4>ajosemichaeloluwatobi@yahoo.com</h4>
-                </div>
-                 <div className='email'>
-                    <img src="https://img.icons8.com/color/50/000000/gmail-new.png" alt='' />
-                    <h4>oluwatrillions44@gmail.com</h4>
-                </div>
-                <div className='email'>
-                    <img src={phone} alt='' />
-                    <h4>+234 802 221 7867</h4>
-                </div>
+            </section>
+            <div className='form-section'>
+                <section className='nav-links'>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href='https://www.linkedin.com/in/oluwatrillions/' target="_blank" rel="noreferrer"><FaLinkedin/></a>
+                            </li>
+                            <li>
+                                <a href='https://www.instagram.com/oluwatrillion/' target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+                            </li>
+                            <li>
+                                <a href='https://twitter.com/Oluwatrillions' target="_blank" rel="noreferrer"><FaTwitterSquare /></a>
+                            </li>
+                            <li> 
+                                <a href='https://github.com/oluwatrillions' target="_blank" rel="noreferrer"><FaGithubSquare /></a>
+                            </li>
+                            <li>
+                                <FaPhoneSquare />
+                                <h4>+234 802 221 7867</h4>
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+                <section className='form'>
+                    <form >
+                        <div className='name-section'>
+                            <main className='name-sec'>
+                                <label htmlFor='firstName'>First Name</label>
+                                <input type='text' name='fname' value={''} />
+                            </main>
+                            <main className='name-sec'> 
+                                <label htmlFor='LastName'>Last Name</label>
+                                <input type='text' name='lname' value={''}/>
+                            </main>
+                        </div>
+                        <div className='email-sec'>
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' name='email' value={''} />
+                        </div>
+                        <div className='msg-sec'>
+                            <label htmlFor='text-area'>Enter your message here</label>
+                            <textarea placeholder='Message'></textarea>
+                        </div>
+                        <button>Submit</button>
+                    </form>
+                </section>
             </div>
-            
         </div>
     )
 }
